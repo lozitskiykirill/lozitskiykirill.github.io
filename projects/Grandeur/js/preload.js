248022947fcc93e53129002f27437f419014e027
KirillLozitskiy <kirilllozitskiy@gmail.com>
2022-03-19 22:09:00 +0200

    new portfolio design

---
+++ projects/Grandeur/js/preload.js
@@ -0,0 +1,7 @@
+// $(window).on('load', function() {
+//     var $preloader = $('#header-preloader'),
+//         $svg_anm   = $preloader.find('.svg_anm');
+//     $svg_anm.fadeOut();
+//     $preloader.addClass("header-preloader-appear");
+//     $preloader.delay(1300).fadeOut('slow');
+// });
